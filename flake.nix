370dcd31dd07a2ca661a7b7efcc66449282528f9
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-07-17 21:23:16 +0700

    add nix flake to track it's own dependencies

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "CS:GO Visualizer Based on Streamlit and Python";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python311;
+
+        pythonEnv = python.withPackages (
+          ps: with ps; [
+            numpy
+            pandas
+            matplotlib
+            plotly
+            seaborn
+            scikit-learn
+            fastparquet
+            pyarrow
+            streamlit
+            igraph
+            leidenalg
+          ]
+        );
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            jupyter
+            pandoc
+            texlive.combined.scheme-full
+          ];
+          buildInputs = [
+            pythonEnv
+            pkgs.nodejs
+          ];
+
+          shellHook = ''
+            echo "Activating Python"
+
+            # Ensure Jupyter uses the correct Python
+            export PATH="${pythonEnv}/bin:$PATH"
+
+
+            echo "Python Registered in PATH"
+          '';
+        };
+      }
+    );
+}
