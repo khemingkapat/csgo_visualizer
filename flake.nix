858f1d423f39033dcb2c62c5b9f3dc1cbe0b26c3
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-07-22 21:21:27 +0700

    using uv2nix to build environment

--- flake.nix
+++ flake.nix
@@ -1,65 +1,86 @@
 {
-  description = "CS:GO Visualizer Based on Streamlit and Python";
+  description = "CS:GO Visualizer dev shell with uv2nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
+      uv2nix,
+      pyproject-nix,
+      pyproject-build-systems,
+      ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        python = pkgs.python311;
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      python = pkgs.python311;
 
-        pythonEnv = python.withPackages (
-          ps: with ps; [
-            pip
-            setuptools
-            wheel
-            numpy
-            pandas
-            matplotlib
-            plotly
-            seaborn
-            scikit-learn
-            fastparquet
-            pyarrow
-            streamlit
-            igraph
-            leidenalg
-          ]
-        );
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
 
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            jupyter
-            pandoc
-            texlive.combined.scheme-full
-          ];
-          buildInputs = [
-            pythonEnv
-            pkgs.nodejs
-          ];
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
 
-          shellHook = ''
-            echo "Activating Python"
+      pythonSet =
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+          (
+            nixpkgs.lib.composeManyExtensions [
+              pyproject-build-systems.overlays.default
+              overlay
+            ]
+          );
 
-            # Ensure Jupyter uses the correct Python
-            export PATH="${pythonEnv}/bin:$PATH"
+      editableOverlay = workspace.mkEditablePyprojectOverlay { root = "$REPO_ROOT"; };
 
+      devPythonSet = pythonSet.overrideScope (nixpkgs.lib.composeManyExtensions [ editableOverlay ]);
 
-            echo "Python Registered in PATH"
-          '';
+      virtualenv = devPythonSet.mkVirtualEnv "dev-env" workspace.deps.all;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          virtualenv
+          pkgs.uv
+        ];
+
+        env = {
+          UV_NO_SYNC = "1";
+          UV_PYTHON = python.interpreter;
+          UV_PYTHON_DOWNLOADS = "never";
         };
-      }
-    );
+        # // nixpkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+        #   LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+        # };
+
+        shellHook = ''
+          unset PYTHONPATH
+          export REPO_ROOT=$(git rev-parse --show-toplevel)
+          echo "✅ uv2nix dev shell ready!"
+        '';
+      };
+    };
 }
