ffb4ab3e8ca799d34878d7e10b6ed802b2f6263f
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-07-29 15:14:51 +0700

    change info

--- page/overview.py
+++ page/overview.py
@@ -5,7 +5,7 @@ from visualizer import Visualizer
 def overview_page():
     st.title("Match Overview")
 
-    if st.session_state.json_data is None:
+    if st.session_state.demo_data is None:
         st.warning("No data available. Please upload a file first.")
         st.session_state.page = "home"
         st.rerun()
@@ -13,10 +13,10 @@ def overview_page():
     # Add match information
     clean_dfs = st.session_state.clean_dfs
     transformed_data = st.session_state.transformed_data
-    match_info = clean_dfs["matches"].iloc[0]
+    match_info = clean_dfs["other_data"].iloc[0]
 
     st.subheader("Match Information")
-    st.info(f"Map: {match_info['map_name']} | Date: {match_info['match_date']}")
+    st.info(f"Map: {match_info['map_name']} | Date: {match_info['tournament_name']}")
 
     fig = Visualizer.plot_round_timeline_plotly(transformed_data["round_results"])
     st.plotly_chart(fig, use_container_width=True)
